24945149e2c9cfcc41e9fccb4bd7c217bf4cbe93
Andrew Kreps <andrew.kreps@gmail.com>
2015-04-27 20:34:47 -0700

    Spaceship

---
+++ racing1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float reverseSpeed = 5.0f;
+	public float turnSpeed = 0.6f;
+
+	private float moveDirection = 0.0f;
+	private float turnDirection = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+						// Input vertical button e.g. up arrow
+		moveDirection = Input.GetAxis("Vertical") * speed;
+		GetComponent<Rigidbody>().AddRelativeForce(0, 0, moveDirection);
+		
+	}
+}
