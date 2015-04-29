d1a88e9055bc9d9f19ae36e78b5075caa78d6aa1
Andrew Kreps <andrew.kreps@gmail.com>
2015-04-28 23:04:54 -0700

    Sprite anim

--- racing1/Assets/Scripts/SpriteAnimation.cs
+++ racing1/Assets/Scripts/SpriteAnimation.cs
@@ -14,6 +14,23 @@ public class SpriteAnimation : MonoBehaviour {
 	private Vector2 frameSize;
 	private Vector2 frameOffset;
 
+	private int idleFrame = 17;
+	private int idleLeft = 1;
+	private int idleRight = 2;
+	private int driveMin = 3;
+	private int driveMax = 4;
+	private int driveLeftMin = 5;
+	private int driveLeftMax = 6;
+	private int driveRightMin = 7;
+	private int driveRightMax = 8;
+	private int spin = 9;
+	private int explosionMin = 10;
+	private int explosionMax = 16;
+
+
+	private static int animIdle = 0;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -33,8 +50,15 @@ public class SpriteAnimation : MonoBehaviour {
 			animationTime++;
 		}
 
+		framePostition.y = 1;
+		int i;
+		for (i = currentFrame; i > columns; i -= rows) {
+			framePostition.y++;
+		}
+		framePostition.x = i - 1;
+
 		frameSize = new Vector2(1.0f / columns, 1.0f / rows);
-		frameOffset = new Vector2(1.0f / columns, 1.0f / rows);
+		frameOffset = new Vector2(framePostition.x / columns, 1.0f - (framePostition.y / rows));
 
 		renderer.material.SetTextureScale("_MainTex", frameSize);
 		renderer.material.SetTextureOffset("_MainTex", frameOffset);
